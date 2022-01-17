cb5ed0d090d2e0ede8a9c8ea0e25a09d2d5a5784
JaeYeonCho <94112330+helloSaltedCaramel@users.noreply.github.com>
2022-01-17 19:36:58 +0900

    Update day1.java

--- Java/variable/day1.java
+++ Java/variable/day1.java
@@ -3,9 +3,9 @@ public class day1 {
 		
 		byte b = 21;
 
-		System.out.println(b+Byte.MAX_VALUE);
+		System.out.println(b+Byte.MAX_VALUE);           // 148
 		
-		System.out.printf("나는 %d살의 대학생입니다",b);
+		System.out.printf("나는 %d살의 대학생입니다",b);      //나는 21살의 대학생입니다
 	}
 
 }
