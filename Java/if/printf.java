6a79bc50551444d68390ef035a2e4b77481632dd
JaeYeonCho <94112330+helloSaltedCaramel@users.noreply.github.com>
2022-01-25 12:49:33 +0900

    Create printf.java

---
+++ Java/if/printf.java
@@ -0,0 +1,3 @@
+System.out.println("당신의 토탈 점수는" + total_score + "입니다 재수강 대상자가 아닙니다.");
+                
+System.out.printf("당신의 토탈점수는 %d점이고, 수학점수는 %d입니다.수학이 60점이상이므로 재수강 대상자가 아닙니다.",total_score,math);
